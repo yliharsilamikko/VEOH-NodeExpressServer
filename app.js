6d3d35d9934371173a0da69209980236c294870d
Ylihärsilä <Mikko.Yliharsila@seamk.fi>
2020-01-17 17:19:06 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+let app = express();
+
+app.use((req, res, next) => {
+    console.log(`path: ${req.path}`);
+    next();
+});
+
+app.use('/TEST', (req, res, next) => {
+    console.log('USE /TEST');
+    next();
+});
+
+app.get('/TEST/', (req, res, next) => {
+    console.log('GET /TEST');
+    next();
+});
+
+app.post('/TEST', (req, res, next) => {
+    console.log('POST /TEST');
+    next();
+});
+
+
+app.get('/', (req, res, next) => {
+    res.send(`Hello world 2`);
+});
+
+app.use((req, res, next) => {
+    res.status(404);
+    res.send(`
+        page not found
+    `);
+});
+
+//Shutdown server CTRL + C in terminal
+app.listen(PORT);
